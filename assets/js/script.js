f52e1faab93b438c473f984c80eeb4c3dc3437a5
Laryssa Ramos <laryssaramos284@gmail.com>
2023-05-18 09:25:40 -0300

    Carrossel de imagens das categorias

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ window.onscroll = () =>{
 // CAROUSEL IMAGES
 
 
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper(".slide-content_1", {
     slidesPerView: 4,
     spaceBetween: 18,
     loop: true,
@@ -31,28 +31,68 @@ var swiper = new Swiper(".slide-content", {
       dynamicBullets: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".btn-next_1",
+      prevEl: ".btn-prev_1",
     },
 
     breakpoints:{
 
         0:{
-            slidesPerView: 1,
-        },
-
-        520:{
             slidesPerView: 2,
         },
 
-        950:{
+        490:{
+            slidesPerView: 2,
+        },
+
+        600:{
+            slidesPerView: 3,
+        },
+
+        1187:{
             slidesPerView: 4,
         },
     }
 
   });
 
+  var swiper = new Swiper(".slide-content_2", {
+    slidesPerView: 4,
+    spaceBetween: 18,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".btn-next_2",
+      prevEl: ".btn-prev_2",
+    },
 
+    breakpoints:{
+
+        0:{
+            slidesPerView: 2,
+        },
+
+        490:{
+            slidesPerView: 2,
+        },
+
+        600:{
+            slidesPerView: 3,
+        },
+
+        1187:{
+            slidesPerView: 4,
+        },
+    }
+
+  });
 
 
 
